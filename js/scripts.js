10fe9ba74710a7588559b6d6f8bb28b610f140dc
AndreNussbaumer <57812689+AndreNussbaumer@users.noreply.github.com>
2022-05-03 11:56:35 +0100

    Blog
    Added blog feature, fixed Instagram link, added a function that fixed opacity when the page was refreshed, added link to the logo.

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,62 @@ function scrollFunction() {
   }
 }
 
+function scrolled() {
+  console.log(window.scrollY)
+  if (window.scrollY) {
+    document.getElementById("navbar").style.backgroundColor = "black";
+  }
+}
+
+scrolled()
+
 function scrollClickContactos() {
-  document.querySelector('.contactos').scrollIntoView({
+  document.querySelector('.contactos-text').scrollIntoView({
   behavior: 'smooth'
   });
 }
 
 function scrollClickSamples() {
-  document.querySelector('.name').scrollIntoView({
+  document.querySelector('.samples-text').scrollIntoView({
   behavior: 'smooth'
   });
 }
 
 function scrollClickBio() {
-  document.querySelector('.content').scrollIntoView({
+  document.querySelector('.bio-container').scrollIntoView({
   behavior: 'smooth'
   });
 }
+
+function scrollClickHome() {
+  scrollTo({
+  top: 0,
+  behavior: 'smooth'
+  });
+}
+
+const popupbutton = document.querySelector('.button')
+const b = document.querySelector('.popup-container')
+const content = document.getElementById('content')
+
+popupbutton.addEventListener('click', () => {
+    b.style.visibility = 'visible'
+})
+
+b.addEventListener('click', (e) => {
+  if(e.target.id !== ('content') && e.target.id !== ('container')) {
+    b.style.visibility = 'hidden'
+  }
+})
+
+
+
+const closepopup = document.querySelector('.popup-close')
+
+closepopup.addEventListener('click', () => {
+  if (b.style.visibility === 'visible') {
+    b.style.visibility = 'hidden'
+  } else {
+    b.style.visibility = 'visible'
+  }
+})
